382546a4241ba98cc26991c3a1822d224682d2be
steven <szapata427@yahoo.com>
2018-11-05 16:01:45 -0500

    finished

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,27 @@
-class Spaceship {}
+let spaceshipId = 0
+
+class Spaceship {
+  constructor(name, crew, phasers, shields, cloaked = false, warpDrive = "disengaged", phasersCharge = "uncharged") {
+    this.name = name
+    this.crew = crew
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = cloaked
+    this.warpDrive = warpDrive
+    this.spaceshipId = ++spaceshipId;
+    if (crew.length > 0) {
+      this.docked = false;
+      const ship = this
+      this.crew.forEach(function(member) {
+        member.currentShip = ship
+      })
+
+    }  else {
+      this.docked = true
+    }
+    this.phasersCharge = phasersCharge
+
+  }
+
+
+}
