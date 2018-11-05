284ba916974a5df0df246b8e1eaac53363be31ac
Bijon Miles <bijonmiles01@gmail.com>
2018-11-05 17:52:46 -0500

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,41 @@
-class CrewMember {}
+let crewMemberId = 0;
+
+class CrewMember {
+  constructor(position, currentShip = 'Looking for a Rig'){
+    this.position = position
+    this.currentShip = currentShip
+    this.id = crewMemberId
+
+  }
+
+  engageWarpDrive() {
+    if  (this.currentShip === 'Looking for a Rig' || this.position !== "Pilot") {
+      return "had no effect"
+    } else {
+      this.currentShip.warpDrive = "engaged";
+    }
+
+  }
+
+  setsInvisibility() {
+    if  (this.currentShip === 'Looking for a Rig' || this.position !== "Defender") {
+      return "had no effect"
+    } else {
+      this.currentShip.cloaked = true
+    }
+  }
+
+  chargePhasers() {
+
+    if  (this.currentShip === 'Looking for a Rig' || this.position !== "Gunner") {
+
+      return "had no effect"
+    } else {
+      // debugger
+      this.currentShip.phasersCharge = "charged"
+      // return this.currentShip.cloaked;
+
+
+    }
+  }
+}
