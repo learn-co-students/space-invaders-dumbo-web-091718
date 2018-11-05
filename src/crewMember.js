e5ffa87d9ab58d6f8c39d41a43167b4db8fb2d3e
Scott Waring <scott.waring@gmail.com>
2018-11-05 16:03:06 -0500

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,36 @@
-class CrewMember {}
+let crewId = 0;
+class CrewMember {
+  constructor(position, currentShip){
+    this.id = ++crewId;
+    this.position = position
+    this.currentShip = 'Looking for a Rig'
+    crewShipJoin["crewMembers"].push(this)
+  }
+
+
+  chargePhasers() {
+    if (this.currentShip !== 'Looking for a Rig' && this.position.toLowerCase() === 'gunner') {
+      this.currentShip.phasers = 'charged'
+    } else {
+      return 'had no effect'
+    }
+  }
+
+  engageWarpDrive() {
+    if (this.currentShip !== 'Looking for a Rig' && this.position.toLowerCase() === 'pilot') {
+      this.currentShip.warpDrive = 'engaged'
+    } else {
+      return 'had no effect'
+    }
+  }
+
+  setsInvisibility() {
+    if (this.currentShip !== 'Looking for a Rig' && this.position.toLowerCase() === 'defender') {
+      this.currentShip.cloaked = true
+    } else {
+      return 'had no effect'
+    }
+  }
+
+}
+//
